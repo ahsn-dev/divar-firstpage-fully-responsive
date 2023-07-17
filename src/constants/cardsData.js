183a50bf4c36866b71f549d3e01fbfa495947307
ahsn.dev <ahsn.dev@gmail.com>
2023-07-17 11:14:27 +0330

    create cardsData as a constant

---
+++ src/constants/cardsData.js
@@ -0,0 +1,99 @@
+export const cardsData = [
+  {
+    imageSrc:
+      "https://github.com/ahsn-dev/divar-firstpage-fully-responsive/blob/3747c2a7e72fc74b490f83d30cd3a7778629c862/src/images/house.jpg",
+    title: "۲۵۰ متر فرمانیه بوستان لوکیشن دیپلمات",
+    price: "۱۹٫۸۰۰٫۰۰۰٫۰۰۰",
+    agency: "آژانس املاک نگین اقدسیه در اقدسیه",
+  },
+  {
+    imageSrc: "src/images/house.jpg",
+    title: "۲۵۰ متر فرمانیه بوستان لوکیشن دیپلمات",
+    price: "۱۹٫۸۰۰٫۰۰۰٫۰۰۰",
+    agency: "آژانس املاک نگین اقدسیه در اقدسیه",
+  },
+  {
+    imageSrc: "src/images/house.jpg",
+    title: "۲۵۰ متر فرمانیه بوستان لوکیشن دیپلمات",
+    price: "۱۹٫۸۰۰٫۰۰۰٫۰۰۰",
+    agency: "آژانس املاک نگین اقدسیه در اقدسیه",
+  },
+  {
+    imageSrc: "src/images/house.jpg",
+    title: "۲۵۰ متر فرمانیه بوستان لوکیشن دیپلمات",
+    price: "۱۹٫۸۰۰٫۰۰۰٫۰۰۰",
+    agency: "آژانس املاک نگین اقدسیه در اقدسیه",
+  },
+  {
+    imageSrc: "src/images/house.jpg",
+    title: "۲۵۰ متر فرمانیه بوستان لوکیشن دیپلمات",
+    price: "۱۹٫۸۰۰٫۰۰۰٫۰۰۰",
+    agency: "آژانس املاک نگین اقدسیه در اقدسیه",
+  },
+  {
+    imageSrc: "src/images/house.jpg",
+    title: "۲۵۰ متر فرمانیه بوستان لوکیشن دیپلمات",
+    price: "۱۹٫۸۰۰٫۰۰۰٫۰۰۰",
+    agency: "آژانس املاک نگین اقدسیه در اقدسیه",
+  },
+  {
+    imageSrc: "src/images/house.jpg",
+    title: "۲۵۰ متر فرمانیه بوستان لوکیشن دیپلمات",
+    price: "۱۹٫۸۰۰٫۰۰۰٫۰۰۰",
+    agency: "آژانس املاک نگین اقدسیه در اقدسیه",
+  },
+  {
+    imageSrc: "src/images/house.jpg",
+    title: "۲۵۰ متر فرمانیه بوستان لوکیشن دیپلمات",
+    price: "۱۹٫۸۰۰٫۰۰۰٫۰۰۰",
+    agency: "آژانس املاک نگین اقدسیه در اقدسیه",
+  },
+  {
+    imageSrc: "src/images/house.jpg",
+    title: "۲۵۰ متر فرمانیه بوستان لوکیشن دیپلمات",
+    price: "۱۹٫۸۰۰٫۰۰۰٫۰۰۰",
+    agency: "آژانس املاک نگین اقدسیه در اقدسیه",
+  },
+  {
+    imageSrc: "src/images/house.jpg",
+    title: "۲۵۰ متر فرمانیه بوستان لوکیشن دیپلمات",
+    price: "۱۹٫۸۰۰٫۰۰۰٫۰۰۰",
+    agency: "آژانس املاک نگین اقدسیه در اقدسیه",
+  },
+  {
+    imageSrc: "src/images/house.jpg",
+    title: "۲۵۰ متر فرمانیه بوستان لوکیشن دیپلمات",
+    price: "۱۹٫۸۰۰٫۰۰۰٫۰۰۰",
+    agency: "آژانس املاک نگین اقدسیه در اقدسیه",
+  },
+  {
+    imageSrc: "src/images/house.jpg",
+    title: "۲۵۰ متر فرمانیه بوستان لوکیشن دیپلمات",
+    price: "۱۹٫۸۰۰٫۰۰۰٫۰۰۰",
+    agency: "آژانس املاک نگین اقدسیه در اقدسیه",
+  },
+  {
+    imageSrc: "src/images/house.jpg",
+    title: "۲۵۰ متر فرمانیه بوستان لوکیشن دیپلمات",
+    price: "۱۹٫۸۰۰٫۰۰۰٫۰۰۰",
+    agency: "آژانس املاک نگین اقدسیه در اقدسیه",
+  },
+  {
+    imageSrc: "src/images/house.jpg",
+    title: "۲۵۰ متر فرمانیه بوستان لوکیشن دیپلمات",
+    price: "۱۹٫۸۰۰٫۰۰۰٫۰۰۰",
+    agency: "آژانس املاک نگین اقدسیه در اقدسیه",
+  },
+  {
+    imageSrc: "src/images/house.jpg",
+    title: "۲۵۰ متر فرمانیه بوستان لوکیشن دیپلمات",
+    price: "۱۹٫۸۰۰٫۰۰۰٫۰۰۰",
+    agency: "آژانس املاک نگین اقدسیه در اقدسیه",
+  },
+  {
+    imageSrc: "src/images/house.jpg",
+    title: "۲۵۰ متر فرمانیه بوستان لوکیشن دیپلمات",
+    price: "۱۹٫۸۰۰٫۰۰۰٫۰۰۰",
+    agency: "آژانس املاک نگین اقدسیه در اقدسیه",
+  },
+];
